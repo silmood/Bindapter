fabefbe767dce9a65d264fc92ad982b57640ef8e
Silmood <dsilmood@gmail.com>
2017-11-22 19:59:15 -0600

    🚀 Main activity was connected with samples

--- app/src/main/java/com/silmood/sbindapter/MainActivity.kt
+++ app/src/main/java/com/silmood/sbindapter/MainActivity.kt
@@ -2,11 +2,20 @@ package com.silmood.sbindapter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showJavaSample(button: View){
+        startActivity(BindapterActivity.buildIntent(this, BindapterActivity.JAVA_SAMPLE))
+    }
+
+    fun showKotlinSample(button: View){
+        startActivity(BindapterActivity.buildIntent(this))
     }
 }
